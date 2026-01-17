315cb8a3610a79d89da4a3a7803b534b49f7d0fd
Pousali Chowdhury <136922307+PousaliChowdhury@users.noreply.github.com>
2026-01-17 17:46:13 +0530

    Update Login.jsx

--- eyeware-store/src/pages/Login.jsx
+++ eyeware-store/src/pages/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:7000/api/auth/login", form);
+      const res = await axios.post("${import.meta.env.VITE_API_URL}/api/auth/login", form);
 
       localStorage.setItem("token", res.data.token);
 
